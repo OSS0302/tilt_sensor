81605b8604845d3ba4732e9c3b11014fb9deed85
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-21 22:06:13 +0900

    092. Stack, Positioned, MediaQuery

--- lib/sensor_app.dart
+++ lib/sensor_app.dart
@@ -5,7 +5,9 @@ class SensorApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
+    final centerX = MediaQuery.of(context).size.width / 2 - 50;
+    final centerY = MediaQuery.of(context).size.height / 2 - 50;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('수평계'),
@@ -13,8 +15,8 @@ class SensorApp extends StatelessWidget {
       body: Stack(
         children: [
           Positioned(
-            left: 100,
-            top: 100,
+            left: centerX,
+            top: centerY,
             child: Container(
               width: 100,
               height: 100,
